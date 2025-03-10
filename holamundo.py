e5c5cb7fcd4dbc3819a4f92f0e3cc2e629b0e470
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-03-10 12:34:59 +0100

    modifico el hola mundo

--- holamundo.py
+++ holamundo.py
@@ -1,4 +1,23 @@
-import time
+import http.server
+import socketserver
+from http import HTTPStatus
+from logging import Handler
 
-print("Hola Mundo")
-time.sleep(3600)  # Mantiene el script vivo por 1 hora
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(HTTPStatus.OK)
+        self.send_header()
+        self.wfile.write(b'Hello, world')
+
+
+
+    def print_hi(name):
+        print(f'Hi, {name}')
+
+
+
+    if __name__ == '__main__':
+        print_hi('PyCon')
+        with socketserver.TCPServer(("", 8000), Handler) as httpd:
+            httpd.serve_forever()

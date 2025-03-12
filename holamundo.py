897d52977cddffff52366b80d58484f7093b53c5
Anthonypradosolis <apradosolis@danielcastelao.org>
2025-03-12 12:17:26 +0100

    borre todo el otro codigo para probar este

--- holamundo.py
+++ holamundo.py
@@ -1,24 +1,18 @@
-import http.server
-import socketserver
-from http import HTTPStatus
-from logging import Handler
+from pytube import YouTube
+from urllib.error import HTTPError
+from pytubefix import YouTube
 
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(b'Hello, world')
+def Download(link):
+    try:
+        yt = YouTube(link)
+        yt = yt.streams.get_highest_resolution()
+        yt.download()
+        print("¡Descarga completada con éxito!")
+    except HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
+    except Exception as e:
+        print(f"Hubo un error al descargar el video del URL proporcionado: {e}")
 
-
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
-
-
-if __name__ == '__main__':
-    print_hi('PyCon')
-    httpd=socketserver.TCPServer(("", 8000), Handler)
-    httpd.serve_forever()
+link = "https://www.youtube.com/watch?v=9NGgLhEVCUk"
+Download(link)
